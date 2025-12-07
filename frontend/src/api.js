876d8b810fbc5e59d4dbeff01168ff70595ee727
B Sai Umesh <saiumesh2003@gmail.com>
2025-12-07 15:27:52 +0530

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE = process.env.REACT_APP_API_URL;
+const API_BASE = process.env.REACT_APP_API_URL || "http://localhost:5000/api";
 
 export const getSnippets = async () => {
   const res = await axios.get(`${API_BASE}/snippets`);
